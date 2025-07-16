58be577f212fe6344c1e3c555b260ddfe4904ef6
Tamilarasan1234567 <kstamilarsankrishnan@gmail.com>
2025-07-16 13:32:16 +0530

    Add files via upload
    My first Commit

---
+++ simple_Project/Oddreven.java
@@ -0,0 +1,16 @@
+package simple_Project;
+
+public class Oddreven {
+
+	public static void main(String[] args) {
+		int n=10;
+		if(n%2==0)
+		{
+			System.out.println("The Given number is even");
+		}
+		else
+			System.out.println("The given number is odd");
+
+	}
+
+}
---
+++ simple_Project/Tamil.java
@@ -0,0 +1,14 @@
+package simple_Project;
+
+public class Tamil {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Tamilarasan");
+		
+	}
+}
+	
+
+
+
